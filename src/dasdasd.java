dc00e5842a13fc1ef750cdbadf148371ba469450
BrasilBrasileira <https://github.com/BrasilBrasileira/JavaProgramming.git>
2022-04-07 23:44:32 +0100

    initial 3 commit

---
+++ src/dasdasd.java
@@ -0,0 +1,27 @@
+
+class lamp {
+
+    // stores the value for light
+    // true if light is on
+    // false if light is off
+    boolean isOn;
+
+    // method to turn on the light
+    void turnOn() {
+        isOn = true;
+        System.out.println("Light on? " + isOn);
+
+    }
+
+    public static void main(String[] args) {
+
+        // create an object of Lamp
+        lamp led = new lamp();
+
+        // access method using object
+        led.turnOn();
+    }
+
+    public void turnOff() {
+    }
+}
